af3cee696a8e81bc9e6cdf0331950bb5367f0b22
Aron Xu <aron@debian.org>
2012-01-25 03:46:26 +0800

    Imported Upstream version 0.3.1

--- src/handler.cpp
+++ src/handler.cpp
@@ -1,21 +1,28 @@
-/*  Copyright (C) 2010~2010 by CSSlayer
-    wengxt@gmail.com 
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+/***************************************************************************
+ *   Copyright (C) 2010~2010 by CSSlayer                                   *
+ *   wengxt@gmail.com                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 
 #include <sunpinyin.h>
+#include <fcitx-utils/utils.h>
+#include <fcitx-utils/log.h>
 #include "handler.h"
+#include "eim.h"
 
 /**
  * @brief handler called while preedit updated
@@ -25,21 +32,14 @@
  **/
 void FcitxWindowHandler::updatePreedit(const IPreeditString* ppd)
 {
-    char *buf_ = eim->CodeInput;
-    TIConvSrcPtr src = (TIConvSrcPtr) (ppd->string());
-    memset(front_src, 0, BUF_SIZE * sizeof(TWCHAR));
-    memset(end_src, 0, BUF_SIZE * sizeof(TWCHAR));
-    
-    memcpy(front_src, src, ppd->caret() * sizeof(TWCHAR));
-    memcpy(end_src, src + ppd->caret() * sizeof(TWCHAR), 
-           (ppd->size() - ppd->caret() + 1) * sizeof(TWCHAR));
-    
-    memset(buf_, 0, MAX_USER_INPUT + 1);
-    
-    WCSTOMBS(buf_, front_src, MAX_USER_INPUT);
-    eim->CaretPos = strlen(buf_);
-    WCSTOMBS(&buf_[strlen(buf_)], end_src, MAX_USER_INPUT);
+    FcitxInstance* instance = owner->owner;
+    FcitxInputState* input = &instance->input;
+    input->bShowCursor = true;
     candidate_flag = true;
+
+    const wstring& codeinput = this->owner->view->getPySegmentor()->getInputBuffer();
+    WCSTOMBS(input->strCodeInput, codeinput.c_str(), MAX_USER_INPUT);
+    input->iCodeInputCount = strlen(input->strCodeInput);
 }
 
 /**
@@ -50,15 +50,7 @@ void FcitxWindowHandler::updatePreedit(const IPreeditString* ppd)
  **/
 void FcitxWindowHandler::updateCandidates(const ICandidateList* pcl)
 {
-    wstring cand_str;
-    for (int i = 0, sz = pcl->size(); i < sz; i++) {
-        const TWCHAR* pcand = pcl->candiString(i);
-        cand_str = pcand;
-        TIConvSrcPtr src = (TIConvSrcPtr)(cand_str.c_str());
-        WCSTOMBS(eim->CandTable[i], (const TWCHAR*) src, MAX_CAND_LEN);
-    }
-
-    eim->CandWordCount = pcl->size();
+    owner->candNum = pcl->total();
 
     candidate_flag = true;
 }
@@ -71,8 +63,12 @@ void FcitxWindowHandler::updateCandidates(const ICandidateList* pcl)
  **/
 void FcitxWindowHandler::commit(const TWCHAR* str)
 {
-    char *buf_ = eim->StringGet;
+    FcitxInstance* instance = owner->owner;
+    FcitxInputState* input = &instance->input;
+    char *buf_ = GetOutputString(&owner->owner->input);
     memset(buf_, 0, MAX_USER_INPUT);
     WCSTOMBS(buf_, str, MAX_USER_INPUT);
     commit_flag = true;
+    input->bShowCursor = false;
 }
+// kate: indent-mode cstyle; space-indent on; indent-width 0;
